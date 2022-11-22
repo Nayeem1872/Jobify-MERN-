36db3ffe1539d014febf25f8f419bb17a2f5c0ec
Md. Hasibul Islam <89178225+Nayeem1872@users.noreply.github.com>
2022-11-22 13:24:59 +0600

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // only when ready to deploy
 // app.use(express.static(path.resolve(__dirname, './client/build')))
-
+ 
 app.use(express.json())
 app.use(helmet())
 app.use(xss())
